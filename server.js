8dc6ed3c3a14c994faa2065cd5786d1c68214333
raymi1996 <heysonpaz96@gmail.com>
2025-07-22 16:39:17 -0500

    Reemplazo de server.js corregido (MediaRoot solucionado)

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
-
 const NodeMediaServer = require('node-media-server');
 
 const config = {
-  logType: 3,
   rtmp: {
     port: 1935,
     chunk_size: 60000,
     gop_cache: true,
-    ping: 60,
-    ping_timeout: 30
+    ping: 30,
+    ping_timeout: 60
   },
   http: {
     port: 8000,
@@ -20,7 +18,9 @@ const config = {
       {
         app: 'live',
         vc: "copy",
+        vcParam: [],
         ac: "aac",
+        acParam: ['-ab', '64k', '-ac', '1', '-ar', '44100'],
         hls: true,
         hlsFlags: '[hls_time=2:hls_list_size=3:hls_flags=delete_segments]',
         dash: false
@@ -29,5 +29,5 @@ const config = {
   }
 };
 
-var nms = new NodeMediaServer(config);
+const nms = new NodeMediaServer(config);
 nms.run();
